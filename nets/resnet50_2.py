e0beae62b419e9ca5a20f6f080fcd9df42a80c2c
김태윤(Kim Taeyoon) <k0278kim@gmail.com>
2025-08-06 13:58:54 +0900

    Update resnet50_2.py

--- nets/resnet50_2.py
+++ nets/resnet50_2.py
@@ -54,15 +54,15 @@ class Bottleneck(nn.Module):
         if use_custom_conv:
             self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)   # 1x1 conv
             self.bn1 = nn.BatchNorm2d(planes)
-            print(f'bottleneck conv1: ({inplanes} -> {planes})')
+            # print(f'bottleneck conv1: ({inplanes} -> {planes})')
             
             self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
             self.bn2 = nn.BatchNorm2d(planes)
-            print(f'bottleneck conv2: ({planes} -> {planes})')
+            # print(f'bottleneck conv2: ({planes} -> {planes})')
             
             self.conv3 = nn.Conv2d(planes, planes * 4 * planes_per_use_custom_planes, kernel_size=1, bias=False)
             self.bn3 = nn.BatchNorm2d(planes * 4 * planes_per_use_custom_planes)
-            print(f'bottleneck conv3: ({planes} -> {planes * 4 * planes_per_use_custom_planes})')
+            # print(f'bottleneck conv3: ({planes} -> {planes * 4 * planes_per_use_custom_planes})')
             
             self.relu = nn.ReLU(inplace=True)
             self.downsample = downsample
@@ -72,21 +72,21 @@ class Bottleneck(nn.Module):
             self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)
 
             self.bn1 = nn.BatchNorm2d(planes)
-            print(f'bottleneck conv1: ({inplanes} -> {planes})')
+            # print(f'bottleneck conv1: ({inplanes} -> {planes})')
             
             self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
             self.bn2 = nn.BatchNorm2d(planes)
-            print(f'bottleneck conv2: ({planes} -> {planes})')
+            # print(f'bottleneck conv2: ({planes} -> {planes})')
             
             self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
             self.bn3 = nn.BatchNorm2d(planes * 4)
-            print(f'bottleneck conv3: ({planes} -> {planes * 4})')
+            # print(f'bottleneck conv3: ({planes} -> {planes * 4})')
             
             self.relu = nn.ReLU(inplace=True)
             self.downsample = downsample
             self.stride = stride
 
-        print('====')
+        # print('====')
 
 
         
@@ -151,17 +151,17 @@ class ResNet2(nn.Module):
 
         if stride != 1 or self.inplanes != planes * block.expansion:
 
-            print(f'use_custom: {use_custom}')
+            # print(f'use_custom: {use_custom}')
 
             if use_custom:
-                print(planes * block.expansion / skip_planes, stride, skip_planes, planes * block.expansion)
+                # print(planes * block.expansion / skip_planes, stride, skip_planes, planes * block.expansion)
                 downsample = nn.Sequential(
                     nn.Conv2d(self.inplanes, skip_planes, kernel_size=1, stride=1, bias=False),
                     Tile(dim=1, reps=int(planes * block.expansion / skip_planes)),
                     nn.BatchNorm2d(planes * block.expansion),
                 )
-                print(f'skip_connections: ({self.inplanes} -> {skip_planes})')
-                print(f'skip_connections: ({skip_planes} -> {planes * block.expansion})')
+                # print(f'skip_connections: ({self.inplanes} -> {skip_planes})')
+                # print(f'skip_connections: ({skip_planes} -> {planes * block.expansion})')
                 
             else:
                 downsample = nn.Sequential(
@@ -169,29 +169,29 @@ class ResNet2(nn.Module):
                     nn.BatchNorm2d(planes * block.expansion)
                 )
 
-                print(f'skip_connections: ({self.inplanes} -> {planes * block.expansion})')
+                # print(f'skip_connections: ({self.inplanes} -> {planes * block.expansion})')
             
        
         layers = []
 
         if use_custom:
-            print(f'<use custom> make block - 1 : 입력 채널 {self.inplanes} / 중간 채널 {use_custom_planes}')
+            # print(f'<use custom> make block - 1 : 입력 채널 {self.inplanes} / 중간 채널 {use_custom_planes}')
             layers.append(block(self.inplanes, use_custom_planes, stride, downsample, use_custom_conv=use_custom, planes_per_use_custom_planes=planes // use_custom_planes))
             self.inplanes = use_custom_planes * block.expansion * (planes // use_custom_planes)
         
         else:
-            print(f'make block - 1 : 입력 채널 {self.inplanes} / 중간 채널 {planes}')
+            # print(f'make block - 1 : 입력 채널 {self.inplanes} / 중간 채널 {planes}')
             layers.append(block(self.inplanes, planes, stride, downsample, use_custom_conv=use_custom))
             self.inplanes = planes * block.expansion
 
         for i in range(1, blocks):
-            print(f'make block - {i + 1} : 입력 채널 {self.inplanes} / 중간 채널 {planes}')
+            # print(f'make block - {i + 1} : 입력 채널 {self.inplanes} / 중간 채널 {planes}')
             layers.append(block(self.inplanes, planes))
 
-        print(f'make block - 1 : 입력 채널 {self.inplanes} / 중간 채널 {planes} / 최종 채널 {planes * block.expansion}')
+        # print(f'make block - 1 : 입력 채널 {self.inplanes} / 중간 채널 {planes} / 최종 채널 {planes * block.expansion}')
         
         
-        print("================")
+        # print("================")
 
         return nn.Sequential(*layers)
 
