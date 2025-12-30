d5efdeaeb4856840883645ec2c24a58435d96de1
k0278kim <k0278kim@gmail.com>
2025-12-30 21:52:57 +0900

    feat: `CUSTOM_CONV_LAYER_INDEX`를 커맨드라인 인자로 설정하도록 변경하고 실행 스크립트를 추가했습니다.

---
+++ run_train_tinet1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "=== 첫 번째 파일 실행 시작 ==="
+python train_tinet_1.py --cusin 1
+
+echo "=== 두 번째 파일 실행 시작 ==="
+python train_tinet_1.py --cusin 2
+
+echo "=== 세 번째 파일 실행 시작 ==="
+python train_tinet_1.py --cusin 3
+
+echo "=== 네 번째 파일 실행 시작 ==="
+python train_tinet_1.py --cusin 4
--- train_tinet_1.py
+++ train_tinet_1.py
@@ -5,13 +5,18 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from nets.resnet50_1_imagenet import ResNet1_imagenet, Bottleneck1_imagenet
 from nets.early_stopping import EarlyStopping
+import argparse
+
+parser = argparse.ArgumentParser(description='ResNet Model2 Training')
+parser.add_argument('--cusin', type=int, default=1, help='custom convolution layer index')
+args = parser.parse_args()
 
 # 하이퍼파라미터
 BATCH_SIZE = 64
 NUM_EPOCHS = 80
 LEARNING_RATE = 1e-3
 NUM_WORKERS = 6
-CUSTOM_CONV_LAYER_INDEX = 4
+CUSTOM_CONV_LAYER_INDEX = args.cusin
 
 # CUDA 설정
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
