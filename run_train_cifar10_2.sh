b73edb20eb0d680dce2dae7569bcdd248433c67d
k0278kim <k0278kim@gmail.com>
2025-12-31 20:35:24 +0900

    feat: 훈련 스크립트를 argparse로 커스텀 컨볼루션 레이어 인덱스를 인자로 받도록 리팩토링하고 실행 쉘 스크립트를 추가했습니다.

---
+++ run_train_cifar10_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "=== train_cifar10_2.py cusin=1 파일 실행 시작 ==="
+python train_cifar10_2.py --cusin 1
+
+echo "=== train_cifar10_2.py cusin=2 파일 실행 시작 ==="
+python train_cifar10_2.py --cusin 2
+
+echo "=== train_cifar10_2.py cusin=3 파일 실행 시작 ==="
+python train_cifar10_2.py --cusin 3
+
+echo "=== train_cifar10_2.py cusin=4 파일 실행 시작 ==="
+python train_cifar10_2.py --cusin 4
