af42079bb005c8c92f5c24fd89f09d5c7d8ab869
k0278kim <k0278kim@gmail.com>
2026-01-02 01:41:30 +0900

    feat: CIFAR-10 학습 스크립트(`run_train_cifar10_2.sh`)의 실행 매개변수를 조정했습니다.

--- run_train_cifar10_2.sh
+++ run_train_cifar10_2.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-echo "=== train_cifar10_2.py cusin=1 파일 실행 시작 ==="
-python train_cifar10_2.py --cusin 1
+echo "=== train_cifar10_1.py cusin=1 model=1 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 1 --model 1
 
-echo "=== train_cifar10_2.py cusin=2 파일 실행 시작 ==="
-python train_cifar10_2.py --cusin 2
+echo "=== train_cifar10_1.py cusin=2 model=1 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 2 --model 1
 
-echo "=== train_cifar10_2.py cusin=3 파일 실행 시작 ==="
-python train_cifar10_2.py --cusin 3
+echo "=== train_cifar10_1.py cusin=3 model=1 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 3 --model 1
 
-echo "=== train_cifar10_2.py cusin=4 파일 실행 시작 ==="
-python train_cifar10_2.py --cusin 4
+echo "=== train_cifar10_1.py cusin=4 model=1 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 4 --model 1
+
+echo "=== train_cifar10_1.py cusin=1 model=2 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 1 --model 2
+
+echo "=== train_cifar10_1.py cusin=2 model=2 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 2 --model 2
+
+echo "=== train_cifar10_1.py cusin=3 model=2 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 3 --model 2
+
+echo "=== train_cifar10_1.py cusin=4 model=2 파일 실행 시작 ==="
+python train_cifar10_1.py --cusin 4 --model 2
