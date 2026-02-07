98fbed168fbcac6ffe4603370f986aa5b078689d
k0278kim <k0278kim@gmail.com>
2026-02-07 13:12:12 +0900

    feat: model layer 변형

--- train_tinet_1.py
+++ train_tinet_1.py
@@ -53,6 +53,7 @@ val_loader = DataLoader(
 
 # 4. 모델 및 도구 설정
 model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes=200, custom_conv_layer_index=CUSTOM_CONV_LAYER_INDEX).to(device)
+model.conv1 = nn.Conv2d(3,64, kernel_size=(3,3), stride=(1,1), padding=(1,1), bias=False)
 
 # 가중치 유실 방지를 위해 구조 변경이 필요한 경우 여기서 수행 (예: 3x3 conv1)
 # model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False).to(device)
